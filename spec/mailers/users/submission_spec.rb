da07be2480e3aaf61fa4900a71eab9e184997a43
Vasili Kachalko <amarant.st@gmail.com>
2021-06-18 15:36:41 +0200

    change user sign in flow copy

--- spec/mailers/users/submission_spec.rb
+++ spec/mailers/users/submission_spec.rb
@@ -21,7 +21,7 @@ describe Users::SubmissionMailer do
     it "renders the headers" do
       expect(mail.subject).to eq("[Queen's Awards for Enterprise] #{subject}")
       expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["no-reply@queens-awards-enterprise.service.gov.uk"])
+      expect(mail.from).to eq(["no-reply@qavs.dcms.gov.uk"])
     end
 
     it "renders the body" do
