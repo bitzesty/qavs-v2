a163651d62596f75b7e1b11e525ca1ae6ea57829
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-06-24 10:50:34 +0100

    Adds question refs and address headings

--- app/forms/award_years/v2022/qavs/qavs_step4.rb
+++ app/forms/award_years/v2022/qavs/qavs_step4.rb
@@ -4,20 +4,24 @@ class AwardYears::V2022::QAEForms
     def qavs_step4
       @qavs_step4 ||= proc do
         header :nominator_details_header, "Details of person making the nomination" do
+          ref "D 1"
         end
 
         text :nominator_title, "Title" do
+          sub_ref "D 1.1"
           classes "sub-question"
           required
           style "tiny"
         end
 
         text :nominator_name, "Name" do
+          sub_ref "D 1.2"
           classes "sub-question"
           required
         end
 
-        address :nominator_address, "" do
+        address :nominator_address, "Address" do
+          sub_ref "D 1.3"
           classes "sub-question"
           required
           sub_fields([
@@ -30,22 +34,26 @@ class AwardYears::V2022::QAEForms
         end
 
         text :nominator_telephone, "Telephone" do
+          sub_ref "D 1.4"
           classes "sub-question"
           style "small"
         end
 
         text :nominator_mobile, "Mobile" do
+          sub_ref "D 1.5"
           classes "sub-question"
           style "small"
         end
 
         text :nominator_email, "Email address" do
+          sub_ref "D 1.6"
           classes "sub-question"
           style "large"
           required
         end
 
         confirm :not_volunteer, "" do
+          sub_ref "D 2"
           required
           text -> do
             %(
@@ -55,6 +63,7 @@ class AwardYears::V2022::QAEForms
         end
 
         confirm :understood_privacy_notice, "" do
+          sub_ref "D 3"
           required
           text -> do
             %(
@@ -64,6 +73,7 @@ class AwardYears::V2022::QAEForms
         end
 
         confirm :group_leader_aware, "" do
+          sub_ref "D 4"
           required
           text -> do
             %(
@@ -73,6 +83,7 @@ class AwardYears::V2022::QAEForms
         end
 
         confirm :entry_confirmation, "Confirmation of nomination" do
+          sub_ref "D 5"
           required
           text -> do
             %(
