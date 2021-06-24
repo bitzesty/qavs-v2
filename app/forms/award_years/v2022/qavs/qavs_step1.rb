a10a6a3b6d35f192b30285e206f7e8636da62e24
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-06-24 10:50:34 +0100

    Adds question refs and address headings

--- app/forms/award_years/v2022/qavs/qavs_step1.rb
+++ app/forms/award_years/v2022/qavs/qavs_step1.rb
@@ -4,20 +4,19 @@ class AwardYears::V2022::QAEForms
     def qavs_step1
       @qavs_step1 ||= proc do
         header :nominee_details_header, "Group Details" do
-          context %(
-            <p><strong>Before nominating, please check that the group meets the eligibility criteria described here and fits the profile described.</strong></p>
-            <p>You can view the questions in advance by downloading a copy of the form: Download Offline Form for Reference. This is only for reference. The form must be completed online.</p>
-          )
+          ref "A 1"
         end
 
         text :nominee_name, "Name of group" do
+          sub_ref "A 1.1"
           classes "sub-question"
           required
           form_hint "It is important that the name is accurate and spelt correctly, as this will appear on the Award certificate if your nomination succeeds. You do not need to add a charity number."
           style "large"
         end
 
-        address :nominee_address, "" do
+        address :nominee_address, "Address of group" do
+          sub_ref "A1.2"
           classes "sub-question"
           required
           sub_fields([
@@ -30,16 +29,19 @@ class AwardYears::V2022::QAEForms
         end
 
         text :nominee_phone, "Telephone number" do
+          sub_ref "A 1.3"
           classes "sub-question"
           style "small"
         end
 
         text :nominee_website, "Website" do
+          sub_ref "A 1.4"
           classes "sub-question"
           style "large"
         end
 
         textarea :social_media, "Social media" do
+          sub_ref "A 1.5"
           classes "sub-question"
           form_hint "Social media accounts if known"
           words_max 100
@@ -47,24 +49,28 @@ class AwardYears::V2022::QAEForms
         end
 
         header :nominee_leader_header, "About the group leader or main contact in the group" do
+          ref "A 2"
           context %(
             <p>This is the person the County Assessment Panel will contact to ask any questions or arrange a visit.</p>
           )
         end
 
         text :nominee_leader_name, "Name of the group leader or main contact in the group" do
+          sub_ref "A 2.1"
           classes "sub-question"
           required
           style "small"
         end
 
         text :nominee_leader_position, "Position held in the group" do
+          sub_ref "A 2.2"
           classes "sub-question"
           required
           style "large"
         end
 
-        address :nominee_leader_address, "" do
+        address :nominee_leader_address, "Address of main contact" do
+          sub_ref "A 2.3"
           classes "sub-question"
           required
           sub_fields([
@@ -77,12 +83,14 @@ class AwardYears::V2022::QAEForms
         end
 
         text :nominee_leader_email, "Email" do
+          sub_ref "A 2.4"
           classes "sub-question"
           required
           style "large"
         end
 
         text :nominee_leader_telephone, "Telephone" do
+          sub_ref "A 2.5"
           classes "sub-question"
           required
           style "small"
