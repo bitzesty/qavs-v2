a7808e1bd8bcc89187c773186b4578d6e3631ef4
Matthew Ford <matt@bitzesty.com>
2021-07-06 15:08:42 +0100

    update session store and db config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,9 +4,9 @@
 # in related environment config files:
 # config/environments/*.rb
 if Rails.env.development? || Rails.env.test?
-  Rails.application.config.session_store :cookie_store, key: "_qae_session_#{Rails.env}"
+  Rails.application.config.session_store :cookie_store, key: "_qavs_session_#{Rails.env}"
 else
-  Rails.application.config.session_store :cookie_store, key: "_qae_session_#{Rails.env}",
+  Rails.application.config.session_store :cookie_store, key: "_qavs_session_#{Rails.env}",
                                                         domain: ENV["COOKIE_DOMAIN"],
                                                         secure: true,
                                                         httponly: true
