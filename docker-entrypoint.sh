fd176b93f526d42c01cab01295de66f7a5a64d34
Matthew Ford <matt@bitzesty.com>
2021-07-19 15:28:00 +0100

    Dont run sidekiq when running db migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 rm -rf tmp/pids
 
-bundle exec sidekiq -C config/sidekiq.yml & # todo use sidekiq.service https://github.com/mperham/sidekiq/blob/master/examples/systemd/sidekiq.service
+if [ -n $REDIS_URL ]; then bundle exec sidekiq -C config/sidekiq.yml &; else echo "Disabling sidekiq"; fi  # todo use sidekiq.service https://github.com/mperham/sidekiq/blob/master/examples/systemd/sidekiq.service
 
 bundle exec puma -C config/puma.rb
