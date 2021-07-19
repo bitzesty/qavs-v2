3b07462e327da38e5c603c45cafea80ce59e0199
Matthew Ford <matt@bitzesty.com>
2021-07-19 15:48:55 +0100

    remove sidekiq for now.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,4 @@
 
 rm -rf tmp/pids
 
-if [ -n $REDIS_URL ]; then bundle exec sidekiq -C config/sidekiq.yml &; else echo "Disabling sidekiq"; fi  # todo use sidekiq.service https://github.com/mperham/sidekiq/blob/master/examples/systemd/sidekiq.service
-
 bundle exec puma -C config/puma.rb
