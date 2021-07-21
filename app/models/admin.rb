64484982920de85f597425806e037c9cba0fb29d
Matthew Ford <matt@bitzesty.com>
2021-07-21 18:34:14 +0100

    Revert "try removing authly"
    This reverts commit 7ba3f7bc0d78f666d48905cff8fbb991b3f035f2.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,7 +6,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable,
+  devise :authy_authenticatable, :database_authenticatable,
          :recoverable, :trackable, :validatable, :confirmable
 
   validates :first_name, :last_name, presence: true
