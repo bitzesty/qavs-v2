bd38d7319b0f8bd90434177d0648a77572949a45
Matthew Ford <matt@bitzesty.com>
2021-07-22 13:40:02 +0100

    debugging before actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < Devise::SessionsController
   after_action :remove_notice, only: :create
 
+  def create
+    actions = _process_action_callbacks.map {|c| {c.filter => c.options} if c.kind == :before }
+    actions.each do |action|
+      Rails.logger.debug action
+    end
+    super
+  end
+
   private
 
   def remove_notice
