8856a9b246a6d386a2c7fe0f1f60fee3b0d65b07
Matthew Ford <matt@bitzesty.com>
2021-07-23 16:14:28 +0100

    action options does not exisit.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < Devise::SessionsController
   after_action :remove_notice, only: :create
 
   def create
-    actions = _process_action_callbacks.map {|c| {c.filter => c.options} if c.kind == :before }
+    actions = _process_action_callbacks.map {|c| c.filter if c.kind == :before }
+    Rails.logger.debug actions
     actions.each do |action|
       Rails.logger.debug action
     end
