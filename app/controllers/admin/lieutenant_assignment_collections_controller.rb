ccf647db105ecb400c3f42f10f315e7e6ba1f5b7
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-07-23 09:52:16 +0100

    adds notice when assigning lieutenancy

--- app/controllers/admin/lieutenant_assignment_collections_controller.rb
+++ app/controllers/admin/lieutenant_assignment_collections_controller.rb
@@ -4,10 +4,18 @@ class Admin::LieutenantAssignmentCollectionsController < Admin::BaseController
     authorize @lieutenant_assignment_collection, :create?
 
     @lieutenant_assignment_collection.subject = current_subject
-    @lieutenant_assignment_collection.save
+    total_checked = @lieutenant_assignment_collection.form_answer_ids.split(",").length
+    if @lieutenant_assignment_collection.save
+      notification = if total_checked > 1
+                       "Groups have"
+                     else
+                       "Group has"
+                     end.concat " been assigned to the Lord Lieutenancy office."
+    end
 
     respond_to do |format|
       format.html do
+        flash[:notice] = notification
         flash[:error] = @lieutenant_assignment_collection.errors.full_messages.to_sentence
         redirect_back(fallback_location: root_path)
       end
