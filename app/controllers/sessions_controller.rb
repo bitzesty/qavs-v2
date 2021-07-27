1fbc74898363fd7ec315431058280a10d3befa71
Matthew Ford <matt@bitzesty.com>
2021-07-27 21:52:49 +0100

    remove session controller create override

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,6 @@
 class SessionsController < Devise::SessionsController
   after_action :remove_notice, only: :create
 
-  def create
-    actions = _process_action_callbacks.map {|c| c.filter if c.kind == :before }
-    actions.each do |action|
-      Rails.logger.debug action
-    end
-    Rails.logger.debug "!!!!!!!!!!!!!!!!"
-    Rails.logger.debug auth_options
-    Rails.logger.debug['HTTP_COOKIE']
-    Rails.logger.debug['warden']
-    self.resource = warden.authenticate!(auth_options)
-    Rails.logger.debug self.resource
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
-
   private
 
   def remove_notice
