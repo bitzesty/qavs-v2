c18f24ccab33d484612dea7720310ac07ab53cfe
Matthew Ford <matt@bitzesty.com>
2021-08-02 19:11:31 +0100

    enable webconsole

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Qae
       end
     end
 
+    config.web_console.development_only = false
     if ENV['WEB_CONSOLE']
-      config.web_console.development_only = false
       config.web_console.permissions = ENV['WEB_CONSOLE']
     end
 
