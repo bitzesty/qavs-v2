968537e2af6a266e8b2b3d94d2c81c438bda90e6
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-03 09:50:17 +0100

    adds enumerize and validations

--- app/models/palace_attendee.rb
+++ app/models/palace_attendee.rb
@@ -5,9 +5,28 @@ class PalaceAttendee < ApplicationRecord
             :title,
             :first_name,
             :last_name,
-            :job_name,
             :address_1,
-            :address_2,
             :postcode,
+            :relationship,
+            :disabled_access,
+            :preferred_date,
+            :alternative_date,
             presence: true
+
+  enumerize :preferred_date, in: %w(
+    any
+    buck_1
+    buck_2
+    buck_3
+    holy
+  )
+
+  enumerize :alternative_date, in: %w(
+    any
+    buck_1
+    buck_2
+    buck_3
+    holy
+    none
+  )
 end
--- db/migrate/20210803084421_add_columns_to_palace_attendees.rb
+++ db/migrate/20210803084421_add_columns_to_palace_attendees.rb
@@ -2,7 +2,7 @@ class AddColumnsToPalaceAttendees < ActiveRecord::Migration[6.0]
   def change
     add_column :palace_attendees, :relationship, :string
     add_column :palace_attendees, :disabled_access, :boolean
-    add_column :palace_attendees, :preferred_option, :string
-    add_column :palace_attendees, :alternative_option, :string
+    add_column :palace_attendees, :preferred_date, :string
+    add_column :palace_attendees, :alternative_date, :string
   end
 end
