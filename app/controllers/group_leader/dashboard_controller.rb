d15030006ede2a97b8f50b9051d62603087d7696
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-09 00:32:52 +0100

    adds content for group leader dashboard

--- app/controllers/group_leader/dashboard_controller.rb
+++ app/controllers/group_leader/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class GroupLeader::DashboardController < GroupLeader::BaseController
   def show
     authorize :group_leader_dashboard, :show?
+    @citation = current_subject.citation
+    # TODO
+    @invite = PalaceInvite.first
   end
 end
