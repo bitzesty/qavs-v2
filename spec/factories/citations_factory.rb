3db506511e593f233a085703bef3345d878f9466
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-09 08:41:11 +0100

    fixes spec

--- spec/factories/citations_factory.rb
+++ spec/factories/citations_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :citation do
-    association :group_leader, factory: :form_answer
+    association :form_answer, factory: :form_answer
     group_name { "Bit Zesty"}
     body { "citation text body" }
   end
