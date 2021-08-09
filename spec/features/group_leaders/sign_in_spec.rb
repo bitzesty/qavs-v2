5be6e708c4ce5be26719ea6fa87aa5998f2313fb
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-09 07:24:12 +0100

    fix spec

--- spec/features/group_leaders/sign_in_spec.rb
+++ spec/features/group_leaders/sign_in_spec.rb
@@ -7,12 +7,15 @@ describe "GroupLeader sign in" do
   let(:group_leader) { create(:group_leader) }
 
   it "allows group_leader to sign in" do
+    create(:citation, group_leader_id: group_leader.id)
+    create(:palace_invite)
+
     visit group_leader_root_path
     fill_in "group_leader_email", with: group_leader.email
     fill_in "group_leader_password", with: "my98ssdkjv9823kds=2"
 
     click_button "Sign in"
 
-    expect(page).to have_content("Group leader dashboard")
+    expect(page).to have_content("Congratulations on being awarded the Queen’s Award for Voluntary Service")
   end
 end
