22402d2c7dc7484d60b1aeb00a6912b21f167963
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-10 16:38:13 +0100

    removes reference to token for invite

--- app/controllers/group_leader/dashboard_controller.rb
+++ app/controllers/group_leader/dashboard_controller.rb
@@ -2,6 +2,12 @@ class GroupLeader::DashboardController < GroupLeader::BaseController
   def show
     authorize :group_leader_dashboard, :show?
     @citation = current_subject.form_answer.citation
-    @invite = current_subject.form_answer.palace_invite
+    @invite = palace_invite
+  end
+
+  private
+
+  def palace_invite
+    current_subject.form_answer.palace_invite || PalaceInvite.create!(form_answer: current_subject.form_answer, email: current_subject.email)
   end
 end
