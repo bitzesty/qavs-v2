2dabf49653c6c7884f8fd851606f66abeaab94a2
Mauricio Andre Cinelli <mauricio.cinelli@bitzesty.com>
2021-08-11 06:04:33 -0300

    Leaving Pundit do its things before redirecting

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,25 +1,28 @@
 class Admin::DashboardController < Admin::BaseController
-  before_action :redirect_to_nominations
-
   def index
     authorize :dashboard, :index?
     @statistics = FormAnswerStatistics::Picker.new(@award_year)
+    redirect_to_nominations
   end
 
   def downloads
     authorize :dashboard, :index?
+    redirect_to_nominations
   end
 
   def totals_by_month
     authorize :dashboard, :totals_by_month?
+    redirect_to_nominations
   end
 
   def totals_by_week
     authorize :dashboard, :totals_by_week?
+    redirect_to_nominations
   end
 
   def totals_by_day
     authorize :dashboard, :totals_by_day?
+    redirect_to_nominations
   end
 
   private
