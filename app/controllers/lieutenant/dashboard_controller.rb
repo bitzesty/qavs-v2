4b1c0fb423bbec3cb9485657d186629e66dbe545
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-16 22:14:12 +0100

    removes commented code

--- app/controllers/lieutenant/dashboard_controller.rb
+++ app/controllers/lieutenant/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class Lieutenant::DashboardController < Lieutenant::BaseController
   def show
     authorize :lieutenant_dashboard, :show?
-    # redirect_to lieutenant_form_answers_url
   end
 end
