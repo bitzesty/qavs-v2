c36d350ef406921e0cbde52d7e9527d7197e0756
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-17 17:38:25 +0100

    adds feature test

--- spec/features/group_leaders/citation_spec.rb
+++ spec/features/group_leaders/citation_spec.rb
@@ -12,20 +12,20 @@ describe "Citation process" do
     login_group_leader(group_leader)
   end
 
-  it 'disables the submit button until the group leader confiorms acceptance' do
+  it 'hides the citation until the group leader confiorms acceptance' do
     click_link "Confirm group name and citation"
 
     expect(page).to have_content("Do you accept the Queen's Award for Voluntary Service on behalf of your group?")
-
   end
 
-  # it "allows group_leader to confirm citation" do
-  #   click_link "Confirm group name and citation"
+  it "allows group_leader to confirm citation" do
+    click_link "Confirm group name and citation"
+    check 'accept-award'
+    
+    expect(page).to have_content("Group name and citation confirmation")
 
-  #   expect(page).to have_content("Group name and citation confirmation")
+    click_button "Submit"
 
-  #   click_button "Submit"
-
-  #   expect(page).to have_selector(".notice", text: "Citation successfully updated!")
-  # end
+    expect(page).to have_selector(".notice", text: "Citation successfully updated!")
+  end
 end
