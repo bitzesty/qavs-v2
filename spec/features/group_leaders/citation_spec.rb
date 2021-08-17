87aa4a19f66ae94fbd886815a116e1ebaaadab22
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-17 12:13:12 +0100

    adds checkbox option to confirm award

--- spec/features/group_leaders/citation_spec.rb
+++ spec/features/group_leaders/citation_spec.rb
@@ -12,13 +12,20 @@ describe "Citation process" do
     login_group_leader(group_leader)
   end
 
-  it "allows group_leader to confirm citation" do
+  it 'disables the submit button until the group leader confiorms acceptance' do
     click_link "Confirm group name and citation"
 
-    expect(page).to have_content("Group name and citation confirmation")
+    expect(page).to have_content("Do you accept the Queen's Award for Voluntary Service on behalf of your group?")
 
-    click_button "Submit"
-
-    expect(page).to have_selector(".notice", text: "Citation successfully updated!")
   end
+
+  # it "allows group_leader to confirm citation" do
+  #   click_link "Confirm group name and citation"
+
+  #   expect(page).to have_content("Group name and citation confirmation")
+
+  #   click_button "Submit"
+
+  #   expect(page).to have_selector(".notice", text: "Citation successfully updated!")
+  # end
 end
