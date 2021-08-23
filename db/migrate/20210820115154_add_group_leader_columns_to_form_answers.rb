4665b1c5b1e4053494f365d66aaf04a9d45b0bce
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-23 11:36:01 +0100

    removes migrations

--- db/migrate/20210820115154_add_group_leader_columns_to_form_answers.rb
+++
@@ -1,9 +0,0 @@
-class AddGroupLeaderColumnsToFormAnswers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :form_answers, :group_leader_name, :string
-    add_column :form_answers, :group_leader_position, :string
-    add_column :form_answers, :group_leader_address, :hstore, default: {}
-    add_column :form_answers, :group_leader_email, :string
-    add_column :form_answers, :group_leader_phone, :string
-  end
-end
