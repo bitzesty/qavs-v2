735ab0b8b0fab42a16ce21ff0b7470eced0ad68e
Matthew Ford <matt@bitzesty.com>
2021-08-26 14:07:27 +0100

    Removing confirmable from LL
    We still need to send them a new account email in the controllers when they are created.
    
    (copy in the old confirmation email)

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
   }
 
   devise_for :lieutenants, controllers: {
-    confirmations: "lieutenants/confirmations",
     passwords: "lieutenants/passwords",
     sessions: "sessions"
   }
