5cd7d431e47e1f74eb102bd244a4c7d9db9c5c32
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-08-26 11:07:08 +0100

    updates success message on changing and creating ll

--- app/controllers/lieutenant/lieutenants_controller.rb
+++ app/controllers/lieutenant/lieutenants_controller.rb
@@ -8,8 +8,8 @@ class Lieutenant::LieutenantsController < Lieutenant::BaseController
     params[:search] ||= LieutenantSearch::DEFAULT_SEARCH
     params[:search].permit!
 
-    @search = LieutenantSearch.new(Lieutenant.from_county(current_lieutenant.ceremonial_county)).
-                             search(params[:search])
+    @search = LieutenantSearch.new(Lieutenant.from_county(current_lieutenant.ceremonial_county))
+                              .search(params[:search])
     @resources = @search.results.page(params[:page])
   end
 
@@ -26,7 +26,7 @@ class Lieutenant::LieutenantsController < Lieutenant::BaseController
     authorize @resource, :create?
 
     if @resource.save
-      flash[:success] = "User successfully created"
+      flash[:success] = "User was successfully created."
       redirect_to lieutenant_lieutenants_url
     else
       render :new
@@ -41,10 +41,10 @@ class Lieutenant::LieutenantsController < Lieutenant::BaseController
     else
       @resource.update_without_password(resource_params)
     end
-
     respond_with :lieutenant, @resource, location: lieutenant_lieutenants_path
+    flash[:success] = "User was successfully updated."
+    flash[:notice] = nil
   end
-
 
   def new
     @resource = Lieutenant.new
