c4e61f1d08ee4479c6c4afc60452a307082265f1
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-09-09 13:34:52 +0100

    changes colour of final paragraph of forms

--- app/forms/award_years/v2022/qavs/qavs_step5.rb
+++ app/forms/award_years/v2022/qavs/qavs_step5.rb
@@ -472,24 +472,24 @@ class AwardYears::V2022::QAEForms
 
         submit "Submit local assessment" do
           notice %(
-            <p class='govuk-hint'>
+            <p class='govuk-body'>
               If you have answered all the questions, you can submit your assessment now. You will be able to edit it any time before [LIEUTENANT_SUBMISSION_ENDS_TIME].
             </p>
-            <p class='govuk-hint'>
+            <p class='govuk-body'>
               If you are not ready to submit yet, you can save your assessment and come back later.
             </p>
           )
           alternative_notices [%(
-              <p class='govuk-hint'>
+              <p class='govuk-body'>
                 You will be able to edit the assessment any time before [LIEUTENANT_SUBMISSION_ENDS_TIME].
               </p>
             ),%(
-            <p class='govuk-hint'>
+            <p class='govuk-body'>
               <strong>
                 Only main lieutenancy office users can submit the assessment.
               </strong>
             </p>
-            <p class='govuk-hint'>
+            <p class='govuk-body'>
               Please save your assessment and inform your main point of contact at the lieutenancy office that it is ready for their review.
             </p>
           )]
