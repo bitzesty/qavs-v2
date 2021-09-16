25e57cfcb833f2511d503617da6d9bfcad8bab70
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-09-16 11:49:24 +0100

    refactor

--- app/controllers/group_leader/accounts_controller.rb
+++ app/controllers/group_leader/accounts_controller.rb
@@ -1,30 +1,3 @@
 class GroupLeader::AccountsController < GroupLeader::BaseController
-  def edit
-    @user = current_group_leader
-    authorize @user, :update_account_details?
-  end
-
-  def update_password
-    @user = current_group_leader
-    authorize @user, :update_account_details?
-    if @user.update_with_password(password_params)
-      flash[:success] = 'Your account details were successfully saved'
-      bypass_sign_in(@user)
-      redirect_to(group_leader_root_path)
-    else
-      flash.alert = 'Error updating your password'
-      @user.reload
-      render :edit
-    end
-  end
-
-  private
-
-  def password_params
-    params.require(:group_leader).permit(
-      :current_password,
-      :password,
-      :password_confirmation
-    )
-  end
+  include AccountDetailsMixin
 end
