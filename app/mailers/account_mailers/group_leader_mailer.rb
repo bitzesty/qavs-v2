60b6dab0b3bf09dfe143354eceb9d70365b4ac82
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-09-22 15:57:41 +0100

    updates group leader nomination mail subject

--- app/mailers/account_mailers/group_leader_mailer.rb
+++ app/mailers/account_mailers/group_leader_mailer.rb
@@ -2,8 +2,8 @@ class AccountMailers::GroupLeaderMailer < AccountMailers::BaseMailer
   def notify(email, name)
     @form_answer = form_answer
     @group_leader_name = name
-
-    subject = "A notification about nomination"
+    
+    subject = "Nomination for Queen’s Award for Voluntary Service"
     send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: email, subject: subject_with_env_prefix(subject)
   end
 end
