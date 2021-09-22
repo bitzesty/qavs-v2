ae7b3747fdaca0e06efd1c6d740afb782be040ee
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-09-22 13:34:35 +0100

    copy update and date format for submission reminder mailer

--- app/mailers/account_mailers/reminder_to_submit_mailer.rb
+++ app/mailers/account_mailers/reminder_to_submit_mailer.rb
@@ -2,7 +2,7 @@ class AccountMailers::ReminderToSubmitMailer < AccountMailers::BaseMailer
   def notify(form_answer_id, collaborator_id)
     @form_answer = FormAnswer.find(form_answer_id)
     @user = @form_answer.user
-    @deadline = Settings.current_submission_deadline.strftime("%A %d %B %Y")
+    @deadline = Settings.current_submission_deadline.strftime("%l %P on %A on #{Settings.current_submission_deadline.day.ordinalize} %B %Y")
     collaborator = User.find(collaborator_id)
 
     subject = "QAVS nomination - reminder to submit"
