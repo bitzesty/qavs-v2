54d9af7e458f55eeb3b62efe0eb9f3d93d66987c
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-10-07 08:50:28 +0100

    removes palace invite spec files

--- spec/factories/palace_invite_forms.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :palace_invite_form do
-    association :invite, factory: :palace_invite
-    attendees_consent { "1" }
-  end
-end
--- spec/models/palace_invite_form_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PalaceInviteForm, type: :model do
-  subject { build(:palace_invite_form) }
-
-  it 'has valid a factory' do
-    expect(subject).to be_valid
-  end
-end
