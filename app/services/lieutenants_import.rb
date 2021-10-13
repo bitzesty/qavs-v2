a36c9b71feca678cbe54e282e95c7320b48de9f1
Vasili Kachalko <amarant.st@gmail.com>
2021-10-13 11:34:58 +0200

    LL import to production

---
+++ app/services/lieutenants_import.rb
@@ -0,0 +1,104 @@
+require 'net/http'
+
+class LieutenantsImport
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+
+    @logs = {
+      warn: [],
+      error: []
+    }
+  end
+
+  def run!
+    log("Start import")
+
+    # "oid"
+    # "email"
+    # "roles"
+    # "encrypted_password"
+    # "ceremonial_county"
+    # "subcommittee"
+
+    CSV.foreach(Rails.root.join(path), headers: true) do |row|
+      oid = row["oid"]
+      log("Importing #{oid}")
+
+      if Lieutenant.find_by_oid(oid)
+        log("Skipping #{oid}, already imported")
+        next
+      end
+
+      Lieutenant.transaction do
+        email = prepare_value(row["email"])
+
+        unless email.present?
+          log("Skipping #{oid}, no email", :error)
+          next
+        end
+
+        u = Lieutenant.new
+        u.email = email
+        u.oid = oid
+        u.first_name = "Imported"
+        u.last_name = "Imported"
+
+        u.password = SecureRandom.hex(20)
+        u.role = "advanced"
+        u.confirmed_at = Time.zone.now
+
+        county = CeremonialCounty.where("LOWER(name) = LOWER(?)", row["ceremonial_county"]).first
+
+        unless county
+          log("Skipping #{oid}, no county found: `#{row["ceremonial_county"]}`", :error)
+          next
+        end
+
+        u.ceremonial_county = county
+        u.save!
+      end
+    end
+
+    log("Finished")
+
+    log("Stats:")
+
+    @logs.each do |k, v|
+      v.each do |err|
+        Rails.logger.send(k, err)
+      end
+    end
+
+    nil
+  end
+
+  private
+
+  def prepare_value(value)
+    value.to_s.strip.gsub(/[\u200B-\u200D\uFEFF]/, '')
+  end
+
+  def log(str, kind = :info)
+    case kind
+    when :error
+      @logs[:error] += [str]
+    when :warn
+      @logs[:warn] += [str]
+    end
+
+    Rails.logger.send(kind, str)
+  end
+
+  # Splits name into First name and last name
+  # Where it's needed
+  def split_name(str)
+    splat = str.split(" ")
+
+    first_name = splat[0].presence || "-"
+    last_name = splat[1..-1].join(" ").presence || "-"
+
+    [first_name, last_name]
+  end
+end
--- lib/tasks/qavs_v1_data_import.rake
+++ lib/tasks/qavs_v1_data_import.rake
@@ -6,4 +6,10 @@ namespace :qavsv1 do
 
     QavsDataImport.new("tmp/nominations_production.csv").run!
   end
+
+  task ll_import: :environment do
+    Rails.logger = Logger.new(STDOUT)
+
+    LieutenantsImport.new("tmp/lieutenants_production.csv").run!
+  end
 end
