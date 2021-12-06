807cd40b99eab8f3c35e2d9e13dbe1d211d33b63
Louis Kirkham <louis.kirkham@bitzesty.com>
2021-12-06 13:46:36 +0000

    adds assessor sign in spec

---
+++ spec/features/assessor/sign_in_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+include Warden::Test::Helpers
+
+Warden.test_mode!
+
+describe "Assessor sign in" do
+  let(:assessor) { create(:assessor) }
+
+  it "allows assessor to sign in" do
+    visit assessor_root_path
+    fill_in "assessor_email", with: assessor.email
+    fill_in "assessor_password", with: "my98ssdkjv9823kds=2"
+
+    click_button "Sign in"
+
+    expect(page).to have_content("Queen’s Award for Voluntary Service national assessors' dashboard")
+  end
+end
