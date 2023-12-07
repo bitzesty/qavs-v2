cb4165cbadfe5811440e514469e25da721479cfa
Vasili Kachalko <amarant.st@gmail.com>
2023-12-07 14:07:47 +0100

    add ability to restore lieutenants for admins

--- app/models/concerns/soft_delete.rb
+++ app/models/concerns/soft_delete.rb
@@ -3,9 +3,15 @@ module SoftDelete
 
   included do
     default_scope { where(deleted: false) }
+
+    scope :deleted, -> { unscoped.where(deleted: true) }
   end
 
   def soft_delete!
     update_column(:deleted, true)
+  end
+
+  def restore!
+    update_column(:deleted, false)
   end
 end
