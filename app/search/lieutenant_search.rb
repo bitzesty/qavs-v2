f6f77f34c8bf237267ee3601187e61a5f176f50e
Nickolay Loshkarev <nickolay.loshkarev@gmail.com>
2024-04-18 16:32:19 +0400

    Add Lord Lieutenancy assigned filter to Lieutenancy office users page

--- app/search/lieutenant_search.rb
+++ app/search/lieutenant_search.rb
@@ -1,11 +1,43 @@
 class LieutenantSearch < Search
   DEFAULT_SEARCH = {
-    sort: "full_name"
+    sort: "full_name",
+    search_filter: {}
   }
 
   include FullNameSort
 
+  def filter_by_assigned_ceremonial_county(scoped_results, value)
+    value = value.map do |v|
+      v == "not_assigned" ? nil : v
+    end
+    scoped_results.where(ceremonial_county_id: value)
+  end
+
   def sort_by_ceremonial_county_name(scoped_results, desc = false)
     scoped_results.joins(:ceremonial_county).order("ceremonial_counties.name #{sort_order(desc)}")
+  end
+
+  class << self
+    def ceremonial_county_options
+      collection_mapping(county_options)
+    end
+
+    private
+
+    def collection_mapping(options)
+      options.map do |k, v|
+        [v[:label], k]
+      end
+    end
+
+    def county_options
+      options = Hash[not_assigned: { label: "Not assigned" }]
+
+      CeremonialCounty.ordered.collect do |county|
+        options[county.id] = { label: county.name }
+      end
+
+      options
+    end
   end
 end
