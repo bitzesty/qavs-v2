60bc94af1776139c1bf20d12dc673966a6af6374
Louis Kirkham <louis.kirkham@bitzesty.com>
2024-07-24 15:58:58 +0100

    Formatting for support letters pdf section Signed-off-by: Louis Kirkham <louis.kirkham@bitzesty.com>
    Adds PDF context for letters of support question
    https://app.asana.com/0/1199154381249427/1207863714230613/f

--- forms/award_years/v2025/qavs/qavs_step3.rb
+++ forms/award_years/v2025/qavs/qavs_step3.rb
@@ -9,19 +9,33 @@ class AwardYears::V2025::QaeForms
         supporters :supporter_letters_list, "" do
           ref "C 1"
           context %(
-            <p class='govuk-body'>Letters of support are an essential part of your nomination, as they help to clarify and explain the impact of the nominated group’s work in the local community. You will need to provide 2 letters of support alongside your nomination.</p>
+            <p class='govuk-body'>Letters of support are an essential part of your nomination, as they help to clarify and explain the impact of the nominated group's work in the local community. You will need to provide 2 letters of support alongside your nomination.</p>
             <p class='govuk-body'>For more information on what letters can cover, please see the <a href="https://kavs.dcms.gov.uk/make-a-nomination/letters-of-support/" target="_blank">Letters of Support page</a> on our website.</p>
             <p class='govuk-body'>Key criteria:</p>
             <ol class='govuk-list govuk-list--number'>
-              <li>letters must be written by individuals who are familiar with the group’s work, for example: a beneficiary, local resident or member of a partner charity</li>
+              <li>letters must be written by individuals who are familiar with the group's work, for example: a beneficiary, local resident or member of a partner charity</li>
               <li>letters must not be written by a volunteer, employee, trustee, or anyone involved in the running of the group</li>
               <li>letters written by the nominator will be ineligible</li>
               <li>each letter should be no more than 500 words</li>
               <li>only 2 letters of support can be submitted</li>
             </ol>
-            <p class='govuk-body'>For each letter uploaded below, please note the letter writer’s relationship to the group, for example: a beneficiary of the group, local resident or member of a partner charity.</p>
+            <p class='govuk-body'>For each letter uploaded below, please note the letter writer's relationship to the group, for example: a beneficiary of the group, local resident or member of a partner charity.</p>
             <p class='govuk-body'><strong>Once uploaded, all files are saved automatically. If you make a mistake and upload the wrong letter, please use the same button to upload the correct file as it will automatically replace the previous version.</strong></p>
           )
+          pdf_context_with_header_blocks [
+            [:normal, %(Letters of support are an essential part of your nomination, as they help to clarify and explain the impact of the nominated group's work in the local community. You will need to provide 2 letters of support alongside your nomination.)],
+            [:normal, %(For more information on what letters can cover, please see the <link href="https://kavs.dcms.gov.uk/make-a-nomination/letters-of-support/" target="_blank">Letters of Support page</link> on our website.)],
+            [:normal, %(Key criteria:
+
+              1. letters must be written by individuals who are familiar with the group's work, for example: a beneficiary, local resident or member of a partner charity
+              2. letters must not be written by a volunteer, employee, trustee, or anyone involved in the running of the group
+              3. letters written by the nominator will be ineligible
+              4. each letter should be no more than 500 words
+              5. only 2 letters of support can be submitted
+            )],
+            [:normal, %(For each letter uploaded below, please note the letter writer's relationship to the group, for example: a beneficiary of the group, local resident or member of a partner charity.)],
+            [:bold, %(Once uploaded, all files are saved automatically. If you make a mistake and upload the wrong letter, please use the same button to upload the correct file as it will automatically replace the previous version.)]
+          ]
           classes "question-support-uploads"
           limit 2
           default 2
