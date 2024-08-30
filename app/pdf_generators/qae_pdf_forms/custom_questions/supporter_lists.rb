180908d529cb03fdd0397efe8bd3aa5228069384
DaniBitZesty <84323332+DaniBitZesty@users.noreply.github.com>
2024-08-30 09:48:57 +0100

    Create a new `ordinal_label` helper method in `FormAnswerHelper`: Refactor supporter_lists question refs are generated in PDF.

--- app/pdf_generators/qae_pdf_forms/custom_questions/supporter_lists.rb
+++ app/pdf_generators/qae_pdf_forms/custom_questions/supporter_lists.rb
@@ -1,4 +1,5 @@
 module QaePdfForms::CustomQuestions::SupporterLists
+  include FormAnswerHelper
   def render_supporters
     entries = form_answer.support_letters.manual
 
@@ -9,31 +10,41 @@ module QaePdfForms::CustomQuestions::SupporterLists
 
   def render_supporters_list(entries)
     entries.each_with_index do |entry, index|
-      entry_index = index == 0 ? "first" : "second"
       ops = [
-        { title: "name_of_the_person_who_wrote_the_#{entry_index}_letter_of_support".to_sym, value: ''},
+        { ref: "C #{index + 1}.1.", title: "name_of_the_person_who_wrote_the_#{ordinal_label(index)}_letter_of_support".to_sym, value: ''},
         { sub_field: "first_name".to_sym, value: entry.first_name },
         { sub_field: "last_name".to_sym, value: entry.last_name },
-        { title: "relationship_to_nominee".to_sym, value: entry.relationship_to_nominee, hint: "For example, a beneficiary of the group, local resident or member of a partner charity." },
+        { ref: "C #{index + 1}.2.", title: "relationship_to_nominee".to_sym, value: entry.relationship_to_nominee, hint: "For example, a beneficiary of the group, local resident or member of a partner charity." },
       ]
 
-      render_supporter(entry, ops, entry_index)
+      render_supporter(entry, ops, index)
     end
   end
 
-  def render_supporter(entry, ops, entry_index)
-    form_pdf.move_down 5.mm
+  def render_supporter(entry, ops, index)
+    form_pdf.text "C #{index + 1}.", style: :bold
+    form_pdf.move_cursor_to form_pdf.cursor + 5.mm
+    form_pdf.indent(13.mm) { form_pdf.text "The #{ordinal_label(index)} letter of support", style: :bold }
+    form_pdf.move_down 2.5.mm
 
     ops.each do |op|
-      form_pdf.text "#{format_label(op[:title])}: ", style: :bold if op[:title]
-      form_pdf.text "#{format_label(op[:sub_field])}: " if op[:sub_field]
-      form_pdf.text(op[:hint], style: :italic) if op[:hint]
-      form_pdf.text op[:value]
-      form_pdf.move_down 2.5.mm
+      if op[:ref]
+        form_pdf.text op[:ref], style: :bold
+        form_pdf.move_cursor_to form_pdf.cursor + 10.mm
+        form_pdf.indent(13.mm) { form_pdf.render_text format_label(op[:title]), style: :bold } if op[:title]
+        form_pdf.indent(13.mm) { form_pdf.render_text op[:hint], style: :italic } if op[:hint]
+        form_pdf.indent(13.mm) { form_pdf.render_text op[:value] }
+      else
+        form_pdf.indent 16.mm do
+          form_pdf.render_text format_label(op[:sub_field])
+          form_pdf.render_text op[:value]
+        end
+      end
+      form_pdf.move_down 5.mm
     end
 
     if entry.is_a?(SupportLetter)
-      render_support_letter(entry, entry_index)
+      render_support_letter(entry, index)
     end
   end
 
@@ -41,16 +52,19 @@ module QaePdfForms::CustomQuestions::SupporterLists
     text.to_s.split('_').join(' ').capitalize
   end
 
-  def render_support_letter(entry, entry_index)
-    form_pdf.text "Upload the #{entry_index} letter of support: ", style: :bold
+  def render_support_letter(entry, index)
+    form_pdf.text "C #{index + 1}.3. Upload the #{ordinal_label(index)} letter of support: ", style: :bold
+    form_pdf.move_down 2.5.mm
 
     if entry.support_letter_attachment.present?
-      form_pdf.base_link_sceleton(
-        form_pdf.attachment_path(entry.support_letter_attachment.attachment, true),
+      form_pdf.indent 12.mm do
+        form_pdf.base_link_sceleton(
+          form_pdf.attachment_path(entry.support_letter_attachment.attachment, true),
         entry.support_letter_attachment.original_filename.truncate(60)
-      )
+        )
+      end
     end
 
-    form_pdf.move_down 2.5.mm
+    form_pdf.move_down 5.mm
   end
 end
