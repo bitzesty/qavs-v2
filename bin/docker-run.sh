69b121329c2be22d1865ddac36094f9d0596c2fb
Louis Kirkham <louis.kirkham@bitzesty.com>
2025-12-15 21:06:08 +0000

    fix: ensure PATH is set in startup script for bundle exec

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Ensure PATH includes vendor/bundle binaries
+export PATH="$HOME/bin:$HOME/vendor/bundle/bin:$HOME/vendor/bundle/ruby/3.2.3/bin:$PATH"
+
 rm -rf tmp/pids
 
 # Start Sidekiq in the background
